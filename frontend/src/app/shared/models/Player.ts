0becac95435178042c5c5dc0d31c8ec2abc9fa11
Sirtus <tslonina@student.agh.edu.pl>
2022-04-04 20:09:13 +0200

    added player model representation

--- frontend/src/app/shared/models/Player.ts
+++ frontend/src/app/shared/models/Player.ts
@@ -22,4 +22,31 @@ export class Player{
     get color(): string {
         return this._color;
     }
+
+    public drawPlayer(ctx: CanvasRenderingContext2D, fieldWidth: number, lineWidth: number): void{
+        const miniFieldWidth = (fieldWidth - lineWidth * 2)/5;
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.lineCap = 'round';
+        let leftUpperX: number =  fieldWidth * this.position.posX + lineWidth;
+        let leftUpperY: number = fieldWidth * this.position.posY + lineWidth;
+        ctx.moveTo(leftUpperX + miniFieldWidth+10, leftUpperY + miniFieldWidth*1.5); 
+        ctx.lineTo(leftUpperX + miniFieldWidth*4-10, leftUpperY + miniFieldWidth*1.5); 
+        ctx.quadraticCurveTo(leftUpperX + miniFieldWidth*4-10, leftUpperY + miniFieldWidth*1.5, leftUpperX + miniFieldWidth*4, leftUpperY + miniFieldWidth*1.5+10); 
+        ctx.lineTo(leftUpperX + miniFieldWidth*4, leftUpperY + miniFieldWidth*3.5 - 10); 
+        ctx.quadraticCurveTo(leftUpperX + miniFieldWidth*4, leftUpperY + miniFieldWidth*3, leftUpperX + miniFieldWidth*4-10, leftUpperY + miniFieldWidth*3.5); 
+        ctx.lineTo(leftUpperX + miniFieldWidth+10, leftUpperY + miniFieldWidth*3.5); 
+        ctx.quadraticCurveTo(leftUpperX + miniFieldWidth+10, leftUpperY + miniFieldWidth*3.5, leftUpperX + miniFieldWidth, leftUpperY + miniFieldWidth*3.5-10); 
+        ctx.lineTo(leftUpperX + miniFieldWidth, leftUpperY + miniFieldWidth*1.5+10); 
+        ctx.quadraticCurveTo(leftUpperX + miniFieldWidth, leftUpperY + miniFieldWidth*1.5+10, leftUpperX + miniFieldWidth+10, leftUpperY + miniFieldWidth*1.5);
+
+        ctx.fill();
+        ctx.stroke();
+        ctx.fillStyle = 'black'
+        ctx.fillRect(leftUpperX + miniFieldWidth+15, leftUpperY + miniFieldWidth*1.5-lineWidth-5, 15 , 10);
+        ctx.fillRect(leftUpperX + miniFieldWidth*4-30, leftUpperY + miniFieldWidth*1.5-lineWidth-5, 15 , 10);
+        ctx.fillRect(leftUpperX + miniFieldWidth+15, leftUpperY + miniFieldWidth*3.5, 15 , 10);
+        ctx.fillRect(leftUpperX + miniFieldWidth*4-30, leftUpperY + miniFieldWidth*3.5, 15 , 10);
+
+      }
 }
