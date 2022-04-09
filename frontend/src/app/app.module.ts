79a06024473909166d799a24332be63a03a6197f
konradst00 <starkombik@gmail.com>
2022-04-09 19:57:23 +0200

    rebase main

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './navigation/app-routing.module';
 import { AppComponent } from './app.component';
-import { MapComponent } from './shared/components/map/map.component';
-import { GameRoomComponent } from './components/game-room/game-room.component';
-import { CreateGameRoomComponent } from './components/create-game-room/create-game-room.component';
-import { JoinGameRoomComponent } from './components/join-game-room/join-game-room.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { RegisterSuccessComponent } from './components/register/register-success/register-success.component';
+import { MapComponent } from './shared/components/map/map.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { StartPageComponent } from './components/start-page/start-page.component';
 import { GameComponent } from './components/game/game.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -25,8 +24,8 @@ import { GameComponent } from './components/game/game.component';
     GameRoomComponent,
     CreateGameRoomComponent,
     JoinGameRoomComponent,
-    GameComponent
-
+    GameComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
