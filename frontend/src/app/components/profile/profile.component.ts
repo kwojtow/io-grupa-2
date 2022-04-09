b1e0c468f31fe4936479dc7546b372f4d43138ff
konradst00 <starkombik@gmail.com>
2022-04-09 22:00:35 +0200

    rebase main

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
+import {RaceMap} from "../../shared/models/RaceMap";
+import {MapComponent} from "../../shared/components/map/map.component";
+import {BehaviorSubject, Subject} from "rxjs";
+import {User} from "../../shared/models/User";
 
 @Component({
   selector: 'app-profile',
@@ -8,7 +12,17 @@ import {Router} from "@angular/router";
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  mapList = new Array<RaceMap>();
+  map: BehaviorSubject<RaceMap>;
+  user: User;
+
+  constructor(private router: Router) {
+    for(let i = 0; i < 4; i ++){
+      this.mapList[i] = MapComponent.getExampleMap(i, i+3);
+    }
+    this.map = new BehaviorSubject<RaceMap>(this.mapList[0]);
+    this.user = new User('User1', 'pwoiegwe', 'User1@email.com', 32, 12345);
+  }
 
   ngOnInit(): void {
   }
@@ -24,4 +38,11 @@ export class ProfileComponent implements OnInit {
   switchToNewMapView() {
     //router.navigate(['newMapView']) // TODO
   }
+
+  changeMap(selectRef: HTMLSelectElement) {
+    this.map.next(this.mapList[selectRef.selectedIndex]);
+  }
+
+
+
 }
