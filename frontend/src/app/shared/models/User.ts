1ae24afc49cc33180d5b86c93d7798b67df19598
konradst00 <starkombik@gmail.com>
2022-04-09 22:00:35 +0200

    rebase main

--- frontend/src/app/shared/models/User.ts
+++ frontend/src/app/shared/models/User.ts
@@ -1,7 +1,60 @@
-export interface User{
-    userId : number;
-    login : string;
-    password: string;
-    email : string;
-    avatar: string;
-    }
+export class User{
+  private _username: string;
+  login : string; // TODO: merge login and username
+  private _password: string;
+  private _email: string;
+  private _rankingPosition: number;
+  private _rankingPoints: number;
+  avatar: string;
+
+
+
+  constructor(username: string, password: string, email: string, rankingPostition: number, rankingPoints: number) {
+    this._username = username;
+    this._password = password;
+    this._email = email;
+    this._rankingPosition = rankingPostition;
+    this._rankingPoints = rankingPoints;
+  }
+
+
+  get username(): string {
+    return this._username;
+  }
+
+  set username(value: string) {
+    this._username = value;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  set password(value: string) {
+    this._password = value;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  set email(value: string) {
+    this._email = value;
+  }
+
+  get rankingPosition(): number {
+    return this._rankingPosition;
+  }
+
+  set rankingPosition(value: number) {
+    this._rankingPosition = value;
+  }
+
+  get rankingPoints(): number {
+    return this._rankingPoints;
+  }
+
+  set rankingPoints(value: number) {
+    this._rankingPoints = value;
+  }
+}
