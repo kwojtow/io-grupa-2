99930022943ce1a7b3f75bdd81beeea78deacf00
Sirtus <tslonina@student.agh.edu.pl>
2022-04-10 23:32:18 +0200

    added hover for available vectors

--- frontend/src/app/shared/models/Player.ts
+++ frontend/src/app/shared/models/Player.ts
@@ -37,14 +37,42 @@ export class Player{
         return this._currentVector;
     }
 
-    public showCurrentVector(ctx: CanvasRenderingContext2D, fieldWidth: number, lineWidth: number): void {
+    public showCurrentVector(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, fieldWidth: number, lineWidth: number, map: RaceMap): void {
         ctx.fillStyle = "#0066ff77";
-
         ctx.fillRect(fieldWidth * (this.currentVector.posX + this.position.posX) + lineWidth,
           fieldWidth * (this.currentVector.posY + this.position.posY) + lineWidth,
           fieldWidth - 2 * lineWidth,
           fieldWidth - 2 * lineWidth);
+
+
     }
+
+
+  public getCursorPosition(canvas: HTMLCanvasElement, event: MouseEvent, raceMap: RaceMap): Vector {
+    const ctx = canvas.getContext('2d');
+    const rect = canvas.getBoundingClientRect()
+    const x = event.offsetX;
+    const y = event.offsetY;
+    // @ts-ignore
+    if (ctx !== null) {
+      const v = this.getIdxPosition(ctx,
+        canvas,
+        rect.width / raceMap.mapWidth, rect.height / raceMap.mapHeight,
+        x, y);
+        return v;
+    }
+    return new Vector(-1,-1);
+  }
+
+  public getIdxPosition(ctx: CanvasRenderingContext2D,
+                        canvas: HTMLCanvasElement,
+                        width: number,
+                        height: number,
+                        cursorPosX: number,
+                        cursorPosY: number): Vector{
+    return new Vector(Math.floor(cursorPosX/width),
+      Math.floor(cursorPosY/height));
+  }
     
     public showAvailableMove(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, fieldWidth: number, lineWidth: number, map: RaceMap): void {
         ctx.fillStyle = "#00ff6677";
@@ -54,7 +82,15 @@ export class Player{
                                   new Vector(this.position.posX, this.position.posY-1), new Vector(this.position.posX-1, this.position.posY-1),
                                   new Vector(this.position.posX+1, this.position.posY-1), new Vector(this.position.posX-1, this.position.posY+1)];
         const currentVector = this.currentVector;
+        
         const fieldWidthWithLine = fieldWidth - 2 * lineWidth;
+        const currentVectorPath = new Path2D();
+
+        currentVectorPath.rect(fieldWidth * (this.currentVector.posX + this.position.posX) + lineWidth,
+          fieldWidth * (this.currentVector.posY + this.position.posY) + lineWidth,
+          fieldWidthWithLine,
+          fieldWidthWithLine);
+        
         for(let vector of availableVectors) {
             let p = new Path2D();
             
@@ -64,6 +100,40 @@ export class Player{
             fieldWidthWithLine);
             availableVectorsPaths.push(p);
             ctx.fill(p);
+        }
+
+        const thisPlayer = this;
+        canvas.onmousemove = function(event) {
+            let v = thisPlayer.getCursorPosition(canvas, event, map);
+            // console.log(v);
+            for(let i = 0; i < availableVectorsPaths.length; ++i) {
+                const path = availableVectorsPaths[i];
+                const vector = availableVectors[i];
+                if(ctx.isPointInPath(path, v.posX*fieldWidth + 2*lineWidth, v.posY*fieldWidth + 2*lineWidth)){
+                    ctx.fillStyle = "#00ff66ff";
+                }
+                else {
+                    ctx.fillStyle = "#00ff6677";
+                }
+                ctx.clearRect(fieldWidth * (currentVector.posX + vector.posX) + lineWidth,
+                fieldWidth * (currentVector.posY + vector.posY) + lineWidth,
+                fieldWidthWithLine,
+                fieldWidthWithLine);
+                ctx.fill(path);
+            }
+
+            if(ctx.isPointInPath(currentVectorPath, v.posX*fieldWidth + 2*lineWidth, v.posY*fieldWidth + 2*lineWidth)){
+                ctx.fillStyle = "#0066ffff";
+            }
+            else {
+                ctx.fillStyle = "#0066ff77";
+            }
+
+            ctx.clearRect(fieldWidth * (thisPlayer.currentVector.posX + thisPlayer.position.posX) + lineWidth,
+            fieldWidth * (thisPlayer.currentVector.posY + thisPlayer.position.posY) + lineWidth,
+            fieldWidthWithLine,
+            fieldWidthWithLine);
+            ctx.fill(currentVectorPath);
         }
 
     }
