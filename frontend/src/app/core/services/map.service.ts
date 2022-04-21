59be5debb79d6baac763514edf067359eb0d2c88
Sirtus <tslonina@student.agh.edu.pl>
2022-04-21 21:40:16 +0200

    added reaction to mouse click

--- frontend/src/app/core/services/map.service.ts
+++ frontend/src/app/core/services/map.service.ts
@@ -57,7 +57,9 @@ export class MapService {
       this.drawObstaclesLines(this._canvas, this._ctx, map);
       if(players.length > 0){
         this.drawPlayers(this._canvas, this._ctx, map, players);
+        this.canvas.style.cursor = 'default';
         this.canvas.removeAllListeners('mousemove');
+        this.canvas.removeAllListeners('click');
         let currentPlayer = players.find(p => p.playerId === player.playerId);
         if(isMyTurn && currentPlayer !== null)
           this.drawPlayerVectors(this._canvas, this._ctx, map, currentPlayer);
@@ -276,6 +278,7 @@ export class MapService {
           else ctx.fillStyle =  (isCurrentVector) ? "#0066ff77" : "#00ff6677";
 
           if(!isOnObstacle){
+            canvas.style.cursor = 'pointer';
             ctx.clearRect(fieldWidth * vector.posX + lineWidth,
             fieldWidth * vector.posY + lineWidth,
             fieldWidth - 2 * lineWidth,
@@ -284,6 +287,48 @@ export class MapService {
           }
           ctx.fillStyle = (isPointInPath && !isOnObstacle) ? "#cc8800" : "#454545";
           ctx.strokeStyle = (isPointInPath && !isOnObstacle) ? "#cc8800" : "#454545";
+
+          if((!isCurrentVector || isPointInPath) && !isOnObstacle) ctx.fill(arrows[i]);
+          ctx.stroke(arrows[i]);
+
+      }
+    }
+  }
+
+
+  private changePosition(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, map: RaceMap,
+                                    player: Player, availableVectorsPaths: Array<Path2D>,
+                                    arrows: Array<Path2D>): void {
+    const lineWidth = this.LINE_WIDTH;
+    const fieldWidth = MapService.getFieldWidth(canvas, map);
+    const availableVectors = player.getAvailableVectors();
+    let onObstacle = this.onObstacle;
+
+    canvas.onclick = function(event) {
+      let v = MapService.getCursorPosition(canvas, event);
+
+      for(let i = availableVectorsPaths.length-1; i >= 0; --i) {
+          
+          const path = availableVectorsPaths[i];
+          const vector = availableVectors[i];
+          const isCurrentVector = vector.equals(player.getCurrentVectorPosition());
+          const isPointInPath = ctx.isPointInPath(path, v.posX*fieldWidth + 2*lineWidth,
+                                                  v.posY*fieldWidth + 2*lineWidth);
+          const isOnObstacle = onObstacle(vector, map);
+
+          if(isPointInPath) ctx.fillStyle =  (isCurrentVector) ? "#001155ff" : "#005511ff";
+          else ctx.fillStyle =  (isCurrentVector) ? "#0066ff77" : "#00ff6677";
+
+          if(!isOnObstacle){
+            canvas.style.cursor = 'grabbing';
+            ctx.clearRect(fieldWidth * vector.posX + lineWidth,
+            fieldWidth * vector.posY + lineWidth,
+            fieldWidth - 2 * lineWidth,
+            fieldWidth - 2 * lineWidth);
+            ctx.fill(path);
+          }
+          ctx.fillStyle = (isPointInPath && !isOnObstacle) ? "#ff9900" : "#454545";
+          ctx.strokeStyle = (isPointInPath && !isOnObstacle) ? "#ff9900" : "#454545";
 
           if((!isCurrentVector || isPointInPath) && !isOnObstacle) ctx.fill(arrows[i]);
           ctx.stroke(arrows[i]);
@@ -330,7 +375,9 @@ export class MapService {
         }
 
       })
+      
       this.highlightAvaliableVectors(canvas, ctx, map, player, availableVectorsPaths, arrows);
+      this.changePosition(canvas, ctx, map, player, availableVectorsPaths, arrows);
   }
 
   get map(): RaceMap {
