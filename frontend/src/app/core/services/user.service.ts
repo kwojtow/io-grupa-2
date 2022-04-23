b69820ae5f35f950b277aec7f72c3162843f1771
konradSt00 <starkombik@gmail.com>
2022-04-23 22:37:38 +0200

    rebase main

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -34,57 +34,70 @@ export class UserService {
   public logUser(user: User) {
     return this.http.post<JwtResponse>('http://localhost:8080/auth/signin', user);
   }
-
   public getUserData(userId: number){
 
-  let httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': "Bearer " + JSON.parse(localStorage.getItem("jwtResponse")).token,
-    })
-  };
+    let httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer " + JSON.parse(localStorage.getItem("jwtResponse")).token,
+      })
+    };
     return this.http.get<User>("http://localhost:8080/user/" + userId, httpOptions);
   }
 
   getCurrentLoggedUserId() : number{
     return JSON.parse(localStorage.getItem("jwtResponse")).id;
   }
-
   public getUser() { // TODO: ??
-    return this.http.get<User>('http://localhost:8080/user');
+    return this.http.get<User>('http://localhost:8080/user', this.getAuthorizationHeaders());
+  }
+  getAuthorizationHeaders() {
+    const jwt = localStorage.getItem('jwtResponse');
+    let requestOptions;
+    if (jwt !== null) {
+      const jwtJson = JSON.parse(jwt);
+      const token = jwtJson['type'] + ' ' + jwtJson['token'];
+      requestOptions = {
+        headers: new HttpHeaders().set('Content-Type', 'application/json')
+          .set('Authorization', token)
+
+      };
+      return requestOptions;
+    }
   }
 
   public getUserStats(userId: number) {
-    return this.http.get<UserStatistics>('http://localhost:8080/statistics/user/' + userId)
+    return this.http.get<UserStatistics>('http://localhost:8080/statistics/user/' + userId, this.getAuthorizationHeaders())
   }
 
   public getUserRanksInfo(userId: number) {
-    return this.http.get<UserRanks>('http://localhost:8080/user/' + userId + '/ranks')
+    return this.http.get<UserRanks>('http://localhost:8080/user/' + userId + '/ranks', this.getAuthorizationHeaders())
   }
 
   //TODO
   public getMapsWithMostWins() {
-    return this.http.get<any>('http://localhost:8080/map/user-wins')
+    return this.http.get<any>('http://localhost:8080/map/user-wins', this.getAuthorizationHeaders())
   }
   //TODO
   public getMapsWithMostGames() {
   }
 
   public getUserMaps(authorId: number): Observable<Array<RaceMap>> {
-    return this.http.get<any>('http://localhost:8080/map?authorId=' + authorId)
+    return this.http.get<any>('http://localhost:8080/map?authorId=' + authorId, this.getAuthorizationHeaders())
       .pipe(map(mapsList => {
-          return mapsList.map((mapResponse: { mapId: number; name: string; userId: number; width: number; height: number; mapStructure: { finishLine: any[]; startLine: any[]; obstacles: any[]; }; }) =>
-            new RaceMap(
-              mapResponse.mapId,
-              mapResponse.name,
-              mapResponse.userId,
-              mapResponse.width,
-              mapResponse.height,
-              mapResponse.mapStructure.finishLine.map(v => new Vector(v.x, v.y)),
-              mapResponse.mapStructure.startLine.map(v => new Vector(v.x, v.y)),
-              mapResponse.mapStructure.obstacles.map(v => new Vector(v.x, v.y))
-            ))
+        return mapsList.map((mapResponse: { mapId: number; name: string; userId: number; width: number; height: number; mapStructure: { finishLine: any[]; startLine: any[]; obstacles: any[]; }; }) =>
+          new RaceMap(
+            mapResponse.mapId,
+            mapResponse.name,
+            mapResponse.userId,
+            mapResponse.width,
+            mapResponse.height,
+            mapResponse.mapStructure.finishLine.map(v => new Vector(v.x, v.y)),
+            mapResponse.mapStructure.startLine.map(v => new Vector(v.x, v.y)),
+            mapResponse.mapStructure.obstacles.map(v => new Vector(v.x, v.y))
+          ))
         }
       ))
   }
+
 }
