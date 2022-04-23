08ab5bebb4fa45a93e0559f332d81220d82935ad
konradSt00 <starkombik@gmail.com>
2022-04-23 22:51:10 +0200

    logout + error 401 handling

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -12,6 +12,7 @@ import { JwtResponse } from 'src/app/shared/models/JwtResponse';
   providedIn: 'root',
 })
 export class UserService {
+  API_URL = 'http://localhost:8080'
   private userUrl: string;
   private user: User;
 
@@ -25,14 +26,14 @@ export class UserService {
     };
 
     return this.http.post<User>(
-      'http://localhost:8080/auth/signup',
+      this.API_URL + '/auth/signup',
       user,
       requestOptions
     );
   }
 
   public logUser(user: User) {
-    return this.http.post<JwtResponse>('http://localhost:8080/auth/signin', user);
+    return this.http.post<JwtResponse>(this.API_URL + '/auth/signin', user);
   }
 
   getAuthorizationHeaders() {
@@ -51,27 +52,27 @@ export class UserService {
   }
 
   public getUser() { // TODO: ??
-    return this.http.get<User>('http://localhost:8080/user', this.getAuthorizationHeaders());
+    return this.http.get<User>(this.API_URL + '/user', this.getAuthorizationHeaders());
   }
 
   public getUserStats(userId: number) {
-    return this.http.get<UserStatistics>('http://localhost:8080/statistics/user/' + userId, this.getAuthorizationHeaders())
+    return this.http.get<UserStatistics>(this.API_URL + '/statistics/user/' + userId, this.getAuthorizationHeaders())
   }
 
   public getUserRanksInfo(userId: number) {
-    return this.http.get<UserRanks>('http://localhost:8080/user/' + userId + '/ranks', this.getAuthorizationHeaders())
+    return this.http.get<UserRanks>(this.API_URL + '/user/' + userId + '/ranks', this.getAuthorizationHeaders())
   }
 
   //TODO
   public getMapsWithMostWins() {
-    return this.http.get<any>('http://localhost:8080/map/user-wins', this.getAuthorizationHeaders())
+    return this.http.get<any>(this.API_URL + '/map/user-wins', this.getAuthorizationHeaders())
   }
   //TODO
   public getMapsWithMostGames() {
   }
 
   public getUserMaps(authorId: number): Observable<Array<RaceMap>> {
-    return this.http.get<any>('http://localhost:8080/map?authorId=' + authorId, this.getAuthorizationHeaders())
+    return this.http.get<any>(this.API_URL + '/map?authorId=' + authorId, this.getAuthorizationHeaders())
       .pipe(map(mapsList => {
         return mapsList.map((mapResponse: { mapId: number; name: string; userId: number; width: number; height: number; mapStructure: { finishLine: any[]; startLine: any[]; obstacles: any[]; }; }) =>
           new RaceMap(
