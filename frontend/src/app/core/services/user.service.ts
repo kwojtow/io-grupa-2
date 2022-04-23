a1a1f141c4d53529bf207f8049a91922005632d1
konradSt00 <starkombik@gmail.com>
2022-04-23 16:42:18 +0200

    rebase main

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { User } from 'src/app/shared/models/User';
+import {UserStatistics} from "../../shared/models/UserStatistics";
+import {UserRanks} from "../../shared/models/UserRanks";
+import {RaceMap} from "../../shared/models/RaceMap";
+import {map, Observable} from "rxjs";
+import { Vector } from 'src/app/shared/models/Vector';
 import { JwtResponse } from 'src/app/shared/models/JwtResponse';
 
 @Injectable({
@@ -8,8 +13,10 @@ import { JwtResponse } from 'src/app/shared/models/JwtResponse';
 })
 export class UserService {
   private userUrl: string;
+  private user: User;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   public addUser(user: User) {
     const requestOptions: Object = {
@@ -27,4 +34,43 @@ export class UserService {
   public logUser(user: User) {
     return this.http.post<JwtResponse>('http://localhost:8080/auth/signin', user);
   }
+
+  public getUser() { // TODO: ??
+    return this.http.get<User>('http://localhost:8080/user');
+  }
+
+  public getUserStats(userId: number) {
+    return this.http.get<UserStatistics>('http://localhost:8080/statistics/user/' + userId)
+  }
+
+  public getUserRanksInfo(userId: number) {
+    return this.http.get<UserRanks>('http://localhost:8080/user/' + userId + '/ranks')
+  }
+
+  //TODO
+  public getMapsWithMostWins() {
+    return this.http.get<any>('http://localhost:8080/map/user-wins')
+  }
+  //TODO
+  public getMapsWithMostGames() {
+  }
+
+  public getUserMaps(authorId: number): Observable<Array<RaceMap>> {
+    return this.http.get<any>('http://localhost:8080/map?authorId=' + authorId)
+      .pipe(map(mapsList => {
+        return mapsList.map((mapResponse: { mapId: number; name: string; userId: number; width: number; height: number; mapStructure: { finishLine: any[]; startLine: any[]; obstacles: any[]; }; }) =>
+          new RaceMap(
+            mapResponse.mapId,
+            mapResponse.name,
+            mapResponse.userId,
+            mapResponse.width,
+            mapResponse.height,
+            mapResponse.mapStructure.finishLine.map(v => new Vector(v.x, v.y)),
+            mapResponse.mapStructure.startLine.map(v => new Vector(v.x, v.y)),
+            mapResponse.mapStructure.obstacles.map(v => new Vector(v.x, v.y))
+          ))
+        }
+      ))
+  }
+
 }
--- frontend/src/app/shared/models/User.ts
+++ frontend/src/app/shared/models/User.ts
@@ -1,24 +1,29 @@
 import {UserStatistics} from "./UserStatistics";
+import {UserRanks} from "./UserRanks";
 
 export class User{
-  private _username: string;
+  private _userId: number;
+  private _login: string;
   private _password: string;
   private _email: string;
   private _statistics: UserStatistics;
+  private _ranks: UserRanks;
 
-  constructor(username: string, password: string, email: string, statistics?: UserStatistics) {
-    this._username = username;
+  constructor( username: string, password: string, email: string, id?: number, statistics?: UserStatistics, ranks?: UserRanks) {
+    this._userId = id;
+    this._login = username;
     this._password = password;
     this._email = email;
     this._statistics = statistics;
+    this._ranks = ranks;
   }
 
-  get username(): string {
-    return this._username;
+  get login(): string {
+    return this._login;
   }
 
-  set username(value: string) {
-    this._username = value;
+  set login(value: string) {
+    this._login = value;
   }
 
   get password(): string {
@@ -43,5 +48,21 @@ export class User{
 
   set statistics(value: UserStatistics) {
     this._statistics = value;
+  }
+
+  get ranks(): UserRanks {
+    return this._ranks;
+  }
+
+  set ranks(value: UserRanks) {
+    this._ranks = value;
+  }
+
+  get userId(): number {
+    return this._userId;
+  }
+
+  set userId(value: number) {
+    this._userId = value;
   }
 }
