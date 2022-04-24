4ce984c88cd4c9594d9a9e7bbab42f2bbfb880a2
konradSt00 <starkombik@gmail.com>
2022-04-24 16:42:04 +0200

    statistics: user maps added

---
+++ frontend/src/app/shared/models/MapResponse.ts
@@ -0,0 +1,21 @@
+import {RaceMap} from "./RaceMap";
+
+export class MapResponse{
+  private _raceMap: RaceMap;
+  private _games: number;
+
+
+  constructor(raceMap: RaceMap, games?: number) {
+    this._raceMap = raceMap;
+    this._games = games;
+  }
+
+
+  get raceMap(): RaceMap {
+    return this._raceMap;
+  }
+
+  get games(): number {
+    return this._games;
+  }
+}
