9905c3a3e27b74685d5d2e67137ce12b9900667f
wojsikora <wojciech.sikora.yt@gmail.com>
2022-04-24 23:21:40 +0200

    add create room

--- frontend/src/app/components/create-room/create-room.component.ts
+++ frontend/src/app/components/create-room/create-room.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-room.component.css']
 })
 export class CreateRoomComponent implements OnInit {
+  maxGamers: number[];
+  roundTime: number[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.maxGamers = Array.from({length: 8}, (_, i) => i + 1);
+    this.roundTime = [3,4,5,6,7,8,9,10,15];
   }
 
 }
