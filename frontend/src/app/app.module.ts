4047ff224cc6398aae502067183f85d773583f1a
Krzysztof Wójtowicz <401284@student.agh.edu.pl>
2022-05-17 18:55:11 +0200

    add ranking

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { StartPageComponent } from './components/start-page/start-page.component
 import { GameComponent } from './components/game/game.component';
 import { JoinGameRoomComponent } from './components/start-page/join-game-room/join-game-room.component';
 import {ProfileComponent} from "./components/profile/profile.component";
+import { RankingComponent } from './components/ranking/ranking.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import {ProfileComponent} from "./components/profile/profile.component";
     GameRoomComponent,
     CreateGameRoomComponent,
     GameComponent,
-    ProfileComponent
+    ProfileComponent,
+    RankingComponent
 
   ],
   imports: [
