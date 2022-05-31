a6ade12ae9d7d21a06d3dbf5961d0848e28ce539
Krzysztof Wójtowicz <401284@student.agh.edu.pl>
2022-05-31 18:31:43 +0200

    add redirectioon

--- frontend/src/app/components/rate-map-modal/rate-map-modal.component.ts
+++ frontend/src/app/components/rate-map-modal/rate-map-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MapService } from 'src/app/core/services/map.service';
 
 @Component({
@@ -13,13 +14,16 @@ export class RateMapModalComponent implements OnInit {
   @Input()
   public mapId : number;
 
-  constructor(private mapService : MapService) { }
+  constructor(private mapService : MapService,
+              private router : Router) { }
 
   ngOnInit(): void {
   }
 
   send(){
-    this.mapService.sendRate(this.mapId, this.currentRate).subscribe();
+    this.mapService.sendRate(this.mapId, this.currentRate).subscribe(() => {
+      this.router.navigate(["/start"]);
+    });
   }
 
 }
