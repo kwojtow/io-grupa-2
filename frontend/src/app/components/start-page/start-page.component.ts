afc7622d7230fa71bf6272bbc2ebb3287aadaf93
wojsikora <wojciech.sikora.yt@gmail.com>
2022-06-12 01:44:41 +0200

    remove local storage after logout

--- frontend/src/app/components/start-page/start-page.component.ts
+++ frontend/src/app/components/start-page/start-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { DataService } from 'src/app/core/services/data.service';
 
 @Component({
@@ -9,7 +10,7 @@ import { DataService } from 'src/app/core/services/data.service';
 export class StartPageComponent implements OnInit {
   userName: string;
 
-  constructor(public dataService: DataService) { }
+  constructor(public dataService: DataService, private router: Router) { }
 
   ngOnInit(): void {
     this.userName = JSON.parse(window.localStorage.getItem('jwtResponse')).username;
@@ -18,6 +19,11 @@ export class StartPageComponent implements OnInit {
 
   changeDialogStatus(){
     this.dataService.updateStatus(true);
+  }
+
+  logout(){
+    window.localStorage.removeItem('jwtResponse');
+    this.router.navigate(['/signin']);
   }
 
 }
